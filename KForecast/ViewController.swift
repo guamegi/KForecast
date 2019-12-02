0dcd30389976576c2bf43e909f1e68722847dca6
Minha Kwon <namisoft@naver.com>
2019-12-02 22:54:44 +0900

    Initial Commit

---
+++ KForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KForecast
+//
+//  Created by 나미콘 on 2019/12/02.
+//  Copyright © 2019 나미콘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
