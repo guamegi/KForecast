5523e2d25ba3cb3781323f5f1a4498e1a01b6f2f
Minha Kwon <namisoft@naver.com>
2019-12-03 21:15:29 +0900

    앱 아이콘 변경

---
+++ KForecast/Common.swift
@@ -0,0 +1,11 @@
+//
+//  Common.swift
+//  KForecast
+//
+//  Created by 나미콘 on 2019/12/03.
+//  Copyright © 2019 나미콘. All rights reserved.
+//
+
+import Foundation
+
+let appKey = "5f9368ba-8a50-4dba-a54a-8d007d69c8d8"
